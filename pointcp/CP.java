cae829aebe5b10cfb034f78c06d81a4b786a8b4c
Shawn Cormier <shawnco789@gmail.com>
2016-09-21 16:49:55 -0400

    Add files via upload
    I can't seem to do it like last time I need the variables to make the calculations maybe you can fix it.

---
+++ pointcp/CP.java
@@ -0,0 +1,15 @@
+abstract class CP{
+/*  public PointCP(char type, double xOrRho, double yOrTheta){
+    this.type = type;
+    this.xOrRho = xOrRho;
+    this.yOrTheta = yOrTheta;
+
+
+  abstract double getDistance(PointCP pointB);
+  abstract void rotatePoint(double rotation);
+  abstract void getX();
+  abstract void getY();
+  abstract void getRho();
+  abstract void getTheta();
+*/
+}
