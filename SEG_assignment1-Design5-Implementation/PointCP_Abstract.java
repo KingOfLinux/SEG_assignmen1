084c8f485469e56c2b124623d62817a1dbc3f656
Shawn Cormier <shawnco789@gmail.com>
2016-09-24 11:07:14 -0400

    Add files via upload

--- SEG_assignment1-Design5-Implementation/PointCP_Abstract.java
+++ SEG_assignment1-Design5-Implementation/PointCP_Abstract.java
@@ -1,6 +1,6 @@
 // This file contains material supporting section 2.9 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
-// license found at www.lloseng.com 
+// license found at www.lloseng.com
 
 /**
  * This class contains instances of coordinates in either polar or
@@ -21,87 +21,63 @@ public abstract class PointCP_Abstract
    * coordinates that are being dealt with.
    */
   public char typeCoord;
-  
+
   /**
    * Contains the current value of X or RHO depending on the type
    * of coordinates.
    */
   public double xOrRho;
-  
+
   /**
    * Contains the current value of Y or THETA value depending on the
    * type of coordinates.
    */
   public double yOrTheta;
-	  
+
   //Instance methods **************************************************
- 
- 
+
+
   public double getX()
   {
-    if(typeCoord == 'C') 
+    if(typeCoord == 'C')
       return xOrRho;
-    else 
+    else
       return (Math.cos(Math.toRadians(yOrTheta)) * xOrRho);
   }
-  
+
   public double getY()
   {
-    if(typeCoord == 'C') 
+    if(typeCoord == 'C')
       return yOrTheta;
-    else 
+    else
       return (Math.sin(Math.toRadians(yOrTheta)) * xOrRho);
   }
-  
+
   public double getRho()
   {
-    if(typeCoord == 'P') 
+    if(typeCoord == 'P')
       return xOrRho;
-    else 
+    else
       return (Math.sqrt(Math.pow(xOrRho, 2) + Math.pow(yOrTheta, 2)));
   }
-  
+
   public double getTheta()
   {
     if(typeCoord == 'P')
       return yOrTheta;
-    else 
+    else
       return Math.toDegrees(Math.atan2(yOrTheta, xOrRho));
   }
-  
-	
+
+
   /**
    * Converts Cartesian coordinates to Polar coordinates.
    */
-  public void convertStorageToPolar()
-  {
-    if(typeCoord != 'P')
-    {
-      //Calculate RHO and THETA
-      double temp = getRho();
-      yOrTheta = getTheta();
-      xOrRho = temp;
-      
-      typeCoord = 'P';  //Change coord type identifier
-    }
-  }
-	
+  public void convertStorageToPolar(){};
   /**
    * Converts Polar coordinates to Cartesian coordinates.
    */
-  public void convertStorageToCartesian()
-  {
-    if(typeCoord != 'C')
-    {
-      //Calculate X and Y
-      double temp = getX();
-      yOrTheta = getY();
-      xOrRho = temp;
-   
-      typeCoord = 'C';	//Change coord type identifier
-    }
-  }
-
+  public void convertStorageToCartesian(){};
   /**
    * Returns information about the coordinates.
    *
@@ -109,7 +85,7 @@ public abstract class PointCP_Abstract
    */
   // public String toString()
   // {
-  //   return "Stored as " + (typeCoord == 'C' 
+  //   return "Stored as " + (typeCoord == 'C'
   //      ? "Cartesian  (" + getX() + "," + getY() + ")"
   //      : "Polar [" + getRho() + "," + getTheta() + "]") + "\n";
   // }
